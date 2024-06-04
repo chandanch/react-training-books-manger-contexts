182bc3ff58c4e7913c6c021e9e57186e65d819c8
chandanch <chandan.cec107@gmail.com>
2024-06-04 06:32:32 +0530

    cb

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import { useEffect, useContext } from "react";
-import BookCreate from "./components/BookCreate";
-import BookList from "./components/BookList";
-import BooksContext from "./context/books";
+import { useEffect, useContext } from 'react';
+import BookCreate from './components/BookCreate';
+import BookList from './components/BookList';
+import BooksContext from './context/books';
 
 function App() {
   const { fetchBooks } = useContext(BooksContext);
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <div className="app">
-      <h1>R eading List</h1>
+      <h1>Reading List</h1>
       <BookList />
       <BookCreate />
     </div>
